854112c777442c74d1203c4e3157e107fd768847
Bill Monsueir <bmonsueir@gmail.com>
2016-07-27 08:11:11 -0700

    api working

--- client/imagesearch.js
+++ client/imagesearch.js
@@ -2,12 +2,14 @@
 Template.imagesearch.helpers({
     results(){
         var data = History.findOne({}, {sort: {createdAt: -1}});
-        console.log(data.terms, data.page);
+
         var offset = data.page * 10;
+        console.log(data.terms, offset);
         //remove before git commit
         
         //return results;
-        var url = "https://api.datamarket.azure.com/Bing/Search/Web?Query=%27" + data.terms + "%27&$skip=" + offset + "$format=json";
+        var url = "https://api.datamarket.azure.com/Bing/Search/Web?Query=%27" + data.terms + "%27&$top=10&$skip=" + offset ;//+ "$format=json";
+
         $.ajax({
             type:'POST',
             url:url,
@@ -16,7 +18,8 @@ Template.imagesearch.helpers({
             }
         }).done(function(data) {
     //alert(data);
-    console.log(data);
+    console.log("data",data);
+    return data;
     });
     }
 });
